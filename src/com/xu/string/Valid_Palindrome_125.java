b67628a0503caad790558f975b730353db7501e6
XuDeveloper <280745397@qq.com>
2018-10-09 23:39:01 +0800

    [ADD]update 1

---
+++ src/com/xu/string/Valid_Palindrome_125.java
@@ -0,0 +1,35 @@
+package com.xu.string;
+
+public class Valid_Palindrome_125 {
+
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            while (i < s.length() && !isValid(s.charAt(i))) {
+                i++;
+            }
+            if (i == s.length()) {
+                return true;
+            }
+            while (j > 0 && !isValid(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                break;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return j <= i;
+    }
+
+    private boolean isValid(char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
+    }
+
+}
