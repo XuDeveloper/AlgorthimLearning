7da455242fbed387205d14cf38af260d67eebfc2
XuDeveloper <280745397@qq.com>
2018-10-14 15:50:51 +0800

    [ADD]update 2

---
+++ src/com/xu/array/Intersection_of_Two_Arrays_349.java
@@ -0,0 +1,29 @@
+package com.xu.array;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Intersection_of_Two_Arrays_349 {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        HashSet<Integer> resultSet = new HashSet<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                resultSet.add(nums2[i]);
+            }
+        }
+        int[] result = new int[resultSet.size()];
+        Iterator<Integer> it = resultSet.iterator();
+        int i = 0;
+        while (it.hasNext()) {
+            result[i] = it.next();
+            i++;
+        }
+        return result;
+    }
+
+}
