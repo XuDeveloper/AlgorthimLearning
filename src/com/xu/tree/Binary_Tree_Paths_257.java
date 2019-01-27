c7d81657e91d8d72ece9c27b11b25df1dbcb3353
XuDeveloper <280745397@qq.com>
2019-01-27 11:35:11 +0800

    [UPDATE]update 1

--- src/com/xu/tree/Binary_Tree_Paths_257.java
+++ src/com/xu/tree/Binary_Tree_Paths_257.java
@@ -5,26 +5,44 @@ import java.util.List;
 
 public class Binary_Tree_Paths_257 {
 
-    private List<String> result = new ArrayList<>();
-
     public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
         if (root == null) {
             return result;
         }
-        findPath(root, root.val + "");
+        if (root.left == null && root.right == null) {
+            result.add(String.valueOf(root.val));
+            return result;
+        }
+        List<String> left = binaryTreePaths(root.left);
+        for (int i = 0; i < left.size(); i++) {
+            result.add(root.val + "->" + left.get(i));
+        }
+        List<String> right = binaryTreePaths(root.right);
+        for (int i = 0; i < right.size(); i++) {
+            result.add(root.val + "->" + right.get(i));
+        }
         return result;
     }
 
-    private void findPath(TreeNode root, String path) {
-        if (root.left == null && root.right == null) {
-            result.add(path);
-        }
-        if (root.left != null) {
-            findPath(root.left, path + "->" + root.left.val);
-        }
-        if (root.right != null) {
-            findPath(root.right, path + "->" + root.right.val);
-        }
-    }
+//    public List<String> binaryTreePaths(TreeNode root) {
+//        if (root == null) {
+//            return result;
+//        }
+//        findPath(root, root.val + "");
+//        return result;
+//    }
+//
+//    private void findPath(TreeNode root, String path) {
+//        if (root.left == null && root.right == null) {
+//            result.add(path);
+//        }
+//        if (root.left != null) {
+//            findPath(root.left, path + "->" + root.left.val);
+//        }
+//        if (root.right != null) {
+//            findPath(root.right, path + "->" + root.right.val);
+//        }
+//    }
 
 }
