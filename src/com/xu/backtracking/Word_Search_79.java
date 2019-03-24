eec44d17fe07e78e04fb856770d3bf51dbacc129
XuDeveloper <280745397@qq.com>
2019-03-24 12:25:03 +0800

    [UPDATE]update 1

--- src/com/xu/backtracking/Word_Search_79.java
+++ src/com/xu/backtracking/Word_Search_79.java
@@ -2,40 +2,50 @@ package com.xu.backtracking;
 
 public class Word_Search_79 {
 
+    // 表示四个方向
+    private int[][] d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private int m, n;
+    private boolean[][] visited;
+
     public boolean exist(char[][] board, String word) {
         if (board == null || board.length == 0 || board[0].length == 0) return false;
         if (word == null || word.length() == 0) return false;
 
-        boolean[][] visited = new boolean[board.length][board[0].length];
+        m = board.length;
+        n = board[0].length;
+        visited = new boolean[m][n];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, word, visited, i, j, 0)) {
+                if (searchWord(board, word, 0, i, j)) {
                     return true;
                 }
             }
         }
-
         return false;
     }
 
-    public boolean dfs(char[][] board, String word, boolean[][] visited, int row, int col, int wi) {
-        if (row < 0 || row > board.length - 1 || col < 0 || col > board[0].length - 1) {
-            return false;
+    // 从board[startx][starty]开始，寻找word[index...word.size()]
+    private boolean searchWord(char[][] board, String word, int index, int startx, int starty) {
+        if (index == word.length() - 1) {
+            return board[startx][starty] == word.charAt(index);
         }
-        if (!visited[row][col] && board[row][col] == word.charAt(wi)) {
-            // return instantly
-            if (wi == word.length() - 1) return true;
-            // traverse unvisited row and col
-            visited[row][col] = true;
-            boolean down = dfs(board, word, visited, row + 1, col, wi + 1);
-            boolean right = dfs(board, word, visited, row, col + 1, wi + 1);
-            boolean up = dfs(board, word, visited, row - 1, col, wi + 1);
-            boolean left = dfs(board, word, visited, row, col - 1, wi + 1);
-            // reset with false if none of above is true
-            visited[row][col] = up || down || left || right;
-            return up || down || left || right;
+        if (board[startx][starty] == word.charAt(index)) {
+            // 从startx，starty出发，向四个方向寻找
+            visited[startx][starty] = true;
+            for (int i = 0; i < 4; i++) {
+                int newx = startx + d[i][0];
+                int newy = starty + d[i][1];
+                if (inArea(newx, newy) && !visited[newx][newy] && searchWord(board, word, index + 1, newx, newy)) {
+                    return true;
+                }
+            }
+            visited[startx][starty] = false;
         }
         return false;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
     }
 
 }
