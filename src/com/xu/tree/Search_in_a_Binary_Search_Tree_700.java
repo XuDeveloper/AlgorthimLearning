56824e95060306972d378bf0e52bfdbebd23686f
XuDeveloper <280745397@qq.com>
2019-06-06 23:37:19 +0800

    [UPDATE]update 1

---
+++ src/com/xu/tree/Search_in_a_Binary_Search_Tree_700.java
@@ -0,0 +1,19 @@
+package com.xu.tree;
+
+public class Search_in_a_Binary_Search_Tree_700 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+
+}
