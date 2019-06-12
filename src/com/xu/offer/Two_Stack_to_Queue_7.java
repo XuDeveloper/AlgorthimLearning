35378db952596b4bacafbd4db6443d1ed6a8deda
XuDeveloper <280745397@qq.com>
2019-06-12 22:31:36 +0800

    [ADD]update 1

--- src/com/xu/offer/Two_Stack_to_Queue_7.java
+++ src/com/xu/offer/Two_Stack_to_Queue_7.java
@@ -4,23 +4,45 @@ import java.util.Stack;
 
 public class Two_Stack_to_Queue_7 {
 
-    Stack<Integer> stack1 = new Stack<Integer>();
-    Stack<Integer> stack2 = new Stack<Integer>();
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
 
-    public void push(int node) {
-        stack1.push(node);
+    /** Initialize your data structure here. */
+    public Two_Stack_to_Queue_7() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
     }
 
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        if (stack1.empty() && stack2.empty()) {
-            throw new RuntimeException("Queue is empty!");
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
         }
-        if (stack2.empty()) {
-            while (!stack1.empty()) {
-                stack2.push(stack1.pop());
-            }
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
         }
         return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (!stack2.isEmpty()) {
+            return stack2.peek();
+        }
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 
 }
