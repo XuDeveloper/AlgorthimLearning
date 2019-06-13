48cea17a1b24242ff084492eb05d3926e2c590ec
XuDeveloper <280745397@qq.com>
2019-06-13 22:30:14 +0800

    [ADD]update 1

--- src/com/xu/tree/Merge_Two_Binary_Trees_617.java
+++ src/com/xu/tree/Merge_Two_Binary_Trees_617.java
@@ -1,15 +1,22 @@
 package com.xu.tree;
 
+// 刷过一遍 （2019.06.13 ）
+
 public class Merge_Two_Binary_Trees_617 {
 
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
         if (t1 != null && t2 != null) {
+            t1.val += t2.val;
             t1.left = mergeTrees(t1.left, t2.left);
             t1.right = mergeTrees(t1.right, t2.right);
-            t1.val += t2.val;
+        }
+        if (t1 != null && t2 == null) {
             return t1;
         }
-        return t1 == null ? t2 : t1;
+        if (t1 == null && t2 != null) {
+            return t2;
+        }
+        return t1;
     }
 
 }
