724e3c28eafb0281be394d4239fe648f8a2ef91d
XuDeveloper <280745397@qq.com>
2019-07-15 21:37:02 +0800

    [ADD]update 1

--- src/com/xu/array/Contains_Duplicate_217.java
+++ src/com/xu/array/Contains_Duplicate_217.java
@@ -1,16 +1,20 @@
 package com.xu.array;
 
-import java.util.HashMap;
+import java.util.HashSet;
+
+// 刷过1遍（2019.07.15 ）
 
 public class Contains_Duplicate_217 {
 
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+        if (nums == null || nums.length == 0) {
+            return false;
         }
-        if (map.size() != nums.length) {
-            return true;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
         }
         return false;
     }
